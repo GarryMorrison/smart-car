c7debab028149eb2ca9befb1172317816986ece7
Garry Morrison <garry@semantic-db.org>
2018-04-27 20:24:20 +0930

    pygame version of panorama-scan.py

--- panorama-scan.py
+++ panorama-scan.py
@@ -13,7 +13,7 @@
 # or maybe this:
 # yup, looks like this one might actually work!
 # though it is having out-of-memory errors on the raspberry pi
-# so best to run this code elsewhere.
+# so best to run the stitch code elsewhere.
 # https://kushalvyas.github.io/stitching.html
 # https://github.com/kushalvyas/Python-Multiple-Image-Stitching
 #
@@ -33,12 +33,13 @@
 import numpy as np
 import time
 import sys
-import cv2
+# import cv2
 import os
+import pygame
 
 
 # try to set up smbus:
-# (this will only work on the raspberry pi, so if it fails we drop back to dummy mode)
+# (this will only work on the raspberry pi)
 try:
     import smbus
     smbus_address = 0x18  # default address
@@ -46,16 +47,48 @@ try:
     bus.open(1)
     have_smbus = True
 except ImportError:
-    print('failed to import smbus, dummy mode on')
-    have_smbus = False
+    print('failed to import smbus')
+    sys.exit(-1)
 
 
-# set up camera:
-cap = cv2.VideoCapture(0)
+# set up camera using opencv:
+# now deprecated, instead we are using pygame, since it is installed by default on the raspberry pi.
+# cap = cv2.VideoCapture(0)
+
+# start up pygame:
+pygame.init()
+
+# set the desired camera size here:
+# camera_size = (320, 240)
+camera_size = (640, 480)
+
+# camera controls:
+hflip = True
+vflip = True
+
+# try to set up the camera using pygame:
+try:
+    import pygame.camera
+    pygame.camera.init()
+    camera_list = pygame.camera.list_cameras()
+    if not camera_list:
+        print('failed to find a camera')
+        sys.exit(-1)
+    else:
+        cam = pygame.camera.Camera(camera_list[0], camera_size)
+        cam.start()
+        cs = cam.get_size()
+        print('camera size: %s' % str(cs))
+except ImportError:
+    print('failed to find a camera')
+    sys.exit(-1)
 
 # number of images to average over:
 count = 20
 # count = 1
+
+# min similarity for image to be considered valid during image averaging:
+IMAGE_SIMILARITY = 0.75
 
 # panorama settings:
 min_angle = 0
@@ -102,6 +135,7 @@ def write_servo(cmd, value):
 
 # assumes, for now, there is no movement at all while trying to take the average:
 # hrmmm.. still has ghost effect. Need to filter using image-simm()
+# NB: opencv version. ie, we haven't written a pygame version of this function.
 def create_average_camera_image(count):
     # let the camera warm up to current angle:
     time.sleep(10)
@@ -127,6 +161,10 @@ def create_average_camera_image(count):
     return int_mean_img
 
 
+# returns the image similarity of two images:
+# 1 for exact match,
+# 0 for completely distinct
+# values in between otherwise
 def image_simm(im1, im2):
     # convert all to float64:
     fim1 = np.float64(im1)
@@ -144,15 +182,15 @@ def image_simm(im1, im2):
     return (2 - wfg) / 2
 
 
+# this still has some minor ghosting!
 def create_simm_average_camera_image(count):
-    # let the camera warm up to current angle:
-    time.sleep(10)
-
-    # create a list of first count frames:
-    img = [cap.read()[1] for i in range(count)]
-
-    # rotate images:
-    rot_img = [cv2.flip(i, -1) for i in img]
+    rot_img = []
+    for _ in range(count):
+        img = cam.get_image()
+        img = pygame.transform.scale(img, camera_size)
+        img = pygame.transform.flip(img, hflip, vflip)
+        img = pygame.surfarray.array3d(img)
+        rot_img.append(img)
 
     # convert all to float64:
     float_img = [np.float64(i) for i in rot_img]
@@ -162,22 +200,21 @@ def create_simm_average_camera_image(count):
     i = 1
     for k in range(1, count):
         tmp_img = float_img[k]
+        # r = image_simm(float_img[0], tmp_img)
         r = image_simm(mean_img, tmp_img)
-        if r > 0.80:
+        if r > IMAGE_SIMILARITY:                  # currently 80% similarity. Maybe another value would work better?
             mean_img += tmp_img
             i += 1
     mean_img /= i
-
-    # find the average:
-    #mean_img = float_img[0]
-    #for k in range(1, count):
-    #    mean_img += float_img[k]
-    #mean_img /= count
+    print('we averaged %s images' % i)
 
     # Convert back to uint8:
     int_mean_img = np.uint8(np.clip(mean_img, 0, 255))
 
-    return int_mean_img
+    # convert back to pygame surface:
+    frame = pygame.surfarray.make_surface(int_mean_img)
+
+    return frame
 
 
 if __name__ == '__main__':
@@ -194,18 +231,20 @@ if __name__ == '__main__':
     # panorama settings:
     # min_angle = 0
     # max_angle = 180
-    # step_angle = 10
+    # step_angle = 5
     for angle in range(min_angle, max_angle + step_angle, step_angle):
         angle = int(constrain(angle, 0, 180))
-        print(angle)
-        write_servo(CMD_SERVO2, angle)
-        time.sleep(2)
-        # img = create_average_camera_image(count)
-        img = create_simm_average_camera_image(count)
-        # cv2.imshow(str(angle), img)
-        cv2.imwrite(dest_dir + '/' + str(angle) + '.png', img)
+        print('angle: %s' % angle)
+        write_servo(CMD_SERVO2, 180 - angle)
 
-    # return camera to 90 degree point:
+        # let the camera warm up to current angle:
+        time.sleep(10)
+        img = create_simm_average_camera_image(count)
+        ## cv2.imshow(str(angle), img)
+        # cv2.imwrite(dest_dir + '/' + str(angle) + '.png', img)
+        pygame.image.save(img, dest_dir + '/' + str(angle) + '.png')
+
+    # return camera to 90 degree position:
     write_servo(CMD_SERVO2, 90)
 
     # tidy up and quit:
